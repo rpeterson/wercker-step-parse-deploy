93c20ff9a7749982146c99c85bbd7b35014b8991
Ryan Peterson <ryan@inovat.com>
2014-06-09 12:33:49 -0400

    execute perms on parse cli

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 curl --progress-bar https://www.parse.com/downloads/cloud_code/parse -o ./parse
+sudo chmod a+x ./parse
 cd deploy
 if [ ! -n "$WERCKER_PARSE_DEPLOY_TARGET" ]; then
   ../parse deploy
