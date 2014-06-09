d8f77931879caeee418ea5b111622d98f9d755df
Ryan Peterson <ryan@inovat.com>
2014-06-09 13:00:40 -0400

    Fix  variable on if condition

--- run.sh
+++ run.sh
@@ -3,8 +3,17 @@
 curl --progress-bar https://www.parse.com/downloads/cloud_code/parse -o ./parse
 sudo chmod a+x ./parse
 cd deploy
+echo "PARSE_DEPLOY_TARGET: $PARSE_DEPLOY_TARGET"
+echo "WERCKER_PARSE_DEPLOY_TARGET: $WERCKER_PARSE_DEPLOY_TARGET"
+
 if [ ! -z "$WERCKER_PARSE_DEPLOY_TARGET" ]; then
   ../parse deploy $WERCKER_PARSE_DEPLOY_TARGET
-else
-  ../parse deploy
+  exit 0
 fi
+
+if [ ! -z "$PARSE_DEPLOY_TARGET" ]; then
+  ../parse deploy $PARSE_DEPLOY_TARGET
+  exit 0
+fi
+
+../parse deploy
